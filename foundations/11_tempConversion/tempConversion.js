c19e17160e93ef4a391e108d1cfc4ef56910c29a
eniola adewale <eni.adewal@gmail.com>
2026-03-06 19:55:01 +0100

    Complete TOP: Temperature Conversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  let temperatureInCelsius = (temperature - 32) * (5 / 9);
+  temperatureInCelsius = temperatureInCelsius.toFixed(1);
+  console.log(temperatureInCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  let temperatureInFahrenheit = temperature * (9 / 5) + 32;
+  temperatureInFahrenheit = temperatureInFahrenheit.toFixed(1);
+  console.log(temperatureInFahrenheit);
 };
 
-// Do not edit below this line
-module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
-};
+convertToCelsius(45);
+convertToFahrenheit(0);
